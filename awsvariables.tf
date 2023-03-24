cc6adac3ba33f064b7a1d1b62ed986d5b3f81657
blahsi <113141616+blahsi@users.noreply.github.com>
2023-03-24 12:09:03 -0500

    Update awsvariables.tf

--- awsvariables.tf
+++ awsvariables.tf
@@ -2,7 +2,7 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "cidr" {
+variable "vpc_cidr" {
  description = "The CIDR block for the VPC"
  type        = string
  default     = "10.0.0.0/16"
